06d1e03aa96ea83704beea3554b9e1a0ee55763e
Bharaniraja-cyber <bharaniraja21@gmail.com>
2025-12-12 12:25:29 +0530

    search function added , and some function will add soon

--- script.js
+++ script.js
@@ -26,10 +26,44 @@ var menu_cancel = document.getElementById("menu_cancel")
 
 hid.addEventListener("click",function(){
     menu.style.left = 0
+    pop1.style.display = "none"
+
 })
 
 
 menu_cancel.addEventListener("click",function(){
     menu.style.left = "-100%"
+    pop1.style.display = ""
+
 })
+
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide_image');
+
+function showSlide(n) {
+    // Hide all
+    slides.forEach(slide => slide.classList.remove('active'));
+    
+    // Loop around
+    if (n >= slides.length) currentSlide = 0;
+    if (n < 0) currentSlide = slides.length - 1;
+    
+    // Show current
+    slides[currentSlide].classList.add('active');
+}
+
+function plusSlides(n) {
+    currentSlide += n;
+    showSlide(currentSlide);
+}
+
+// Auto-play every 4 seconds (optional - remove if you don't want auto slide)
+setInterval(() => {
+    plusSlides(1);
+}, 4000);
+
+// Start with first slide
+showSlide(currentSlide);
 
